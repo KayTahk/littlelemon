c24b27c585629e23f3018663f6ba4cae9b579907
KayTahk <kayleightahk@gmail.com>
2023-02-09 20:15:30 -0500

    Added HTML tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import Nav from './Nav.js';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </React.Fragment>
   );
 }
 

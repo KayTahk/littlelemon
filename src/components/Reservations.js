8977f0ca39796667146dcf99fc47fa4b79010593
KayTahk <kayleightahk@gmail.com>
2023-02-17 04:35:03 -0500

    added login page and reservation page basic functionality

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,7 +1,123 @@
-function Reservations() {
-    return (
-        <div className="content">Reservations is under construction</div>
-    );
-}
+import { useState } from "react";
 
-export default Reservations;
+
+
+export default function Reservations() {
+
+    const availableTimes = useState({
+        dropdownvalue: "17:00"
+    });
+
+  const [bookingForm, setBookingForm] = useState({
+    name: "",
+    email: "",
+    phonenumber: "",
+    date: "",
+    time: "",
+    numberofguests: "",
+    occasion: "",
+  });
+
+  const handleChange = (event) => {
+    setBookingForm({ ...bookingForm, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(bookingForm);
+    setBookingForm({ name: "", email: "", phonenumber: "",date: "", time: "", numberofguests: "", occasion: "" });
+  };
+
+  return (
+    <div className="form-container">
+        <div className="form-header">
+          <h3>Reserve a Table</h3>
+          <h4>Little Lemon Restaurant Chicago</h4>
+          <h5>1441 Tator Patch Road, Chicago, IL 60605</h5>
+          <h6><b>Please Note:</b> For group reservations over 15 guests please call (555) 555-5555. Online reservations are for indoor dining only. Reservations will not be held after the 15 minute late arrival grace period.</h6>
+        </div>
+      <form onSubmit={handleSubmit}>
+        <div className="Field">
+        <p>Date:</p>
+          <input
+            type="date"
+            name="date"
+            placeholder="Date"
+            value={bookingForm.date}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="Field">
+        <p>Time:</p>
+          <select
+            type="time"
+            name="time"
+            placeholder="Time"
+            value={bookingForm.time}
+            onChange={handleChange}
+          >
+            {availableTimes.map((time) => (
+                <option value={time.dropdownvalue}
+                >{time.dropdownvalue}</option>
+                ))}</select>
+        </div>
+        <div className="Field">
+        <p>Number of Guests:</p>
+          <input
+            type="number"
+            name="numberofguests"
+            placeholder="Number of Guests"
+            value={bookingForm.numberofguests}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="Field">
+        <p>Occasion:</p>
+          <select
+            type="text"
+            name="occasion"
+            placeholder="Occasion"
+            value={bookingForm.occasion}
+            onChange={handleChange}>
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
+          <option value="Engagement">Engagement</option>
+          </select>
+        </div>
+        <div className="Field">
+        <p>Name:</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={bookingForm.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="Field">
+        <p>Email:</p>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={bookingForm.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="Field">
+        <p>Phone Number:</p>
+          <input
+            type="number"
+            name="phonenumber"
+            placeholder="Phone Number"
+            value={bookingForm.phonenumber}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button>Make Your Reservation</button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,138 @@
+import { useState } from "react";
+
+const PasswordErrorMessage = () => {
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
+
 function Login() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+
+  const getIsFormValid = () => {
     return (
-        <div className="content">Login to account is under construction</div>
+        firstName &&
+        email &&
+        password.value.length >= 8
     );
+};
+
+  const clearForm = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword({ value: "", isTouched: false, })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Account created!");
+    clearForm();
+  };
+
+  const [isSignupSelected, setIsSignupSelected] = useState(false);
+    function handleSignupOnClick() {
+        setIsSignupSelected(!isSignupSelected);
+    }
+
+  return (
+    <><div className={isSignupSelected ? "login-form-container hidden" : "login-form-container"}>
+          <form onSubmit={handleSubmit}>
+              <fieldset>
+                  <h2>Login</h2>
+                  <div className="Field">
+                      <label>
+                          <p>Email address:<sup>*</sup></p>
+                      </label>
+                      <input
+                          type="text"
+                          placeholder="Email Address"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)} />
+                  </div>
+                  <div className="Field">
+                      <label>
+                          <p>Password:<sup>*</sup></p>
+                      </label>
+                      <input
+                          value={password.value}
+                          type="password"
+                          onChange={(e) => { setPassword({ ...password, value: e.target.value }); } }
+                          onBlur={() => {
+                              setPassword({ ...password, isTouched: true });
+                          } }
+                          placeholder="Password" />
+                      {password.isTouched && password.value.length < 8 ? (<PasswordErrorMessage />) : null}
+                  </div>
+                  <button
+                      type="submit"
+                      disabled={!getIsFormValid()}>
+                      Login
+                  </button>
+              </fieldset>
+          </form>
+          <button className="login" onClick={handleSignupOnClick}>Don't have an Account? Signup!</button>
+      </div><div className={isSignupSelected ? "signup-form-container expanded" : "signup-form-container"}>
+              <form onSubmit={handleSubmit}>
+                  <fieldset>
+                      <h2>Sign Up</h2>
+                      <div className="Field">
+                          <label>
+                              <p>First name:<sup>*</sup></p>
+                          </label>
+                          <input
+                              type="text"
+                              placeholder="First Name"
+                              value={firstName}
+                              onChange={(e) => setFirstName(e.target.value)} />
+                      </div>
+                      <div className="Field">
+                          <label><p>Last name:</p></label>
+                          <input
+                              type="text"
+                              placeholder="Last Name"
+                              value={lastName}
+                              onChange={(e) => setLastName(e.target.value)} />
+                      </div>
+                      <div className="Field">
+                          <label>
+                              <p>Email address:<sup>*</sup></p>
+                          </label>
+                          <input
+                              type="text"
+                              placeholder="Email Address"
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)} />
+                      </div>
+                      <div className="Field">
+                          <label>
+                              <p>Password:<sup>*</sup></p>
+                          </label>
+                          <input
+                              value={password.value}
+                              type="password"
+                              onChange={(e) => { setPassword({ ...password, value: e.target.value }); } }
+                              onBlur={() => {
+                                  setPassword({ ...password, isTouched: true });
+                              } }
+                              placeholder="Password" />
+                          {password.isTouched && password.value.length < 8 ? (<PasswordErrorMessage />) : null}
+                      </div>
+                      <button
+                          type="submit"
+                          disabled={!getIsFormValid()}>
+                          Create account
+                      </button>
+                  </fieldset>
+              </form>
+          </div></>
+  );
 }
 
 export default Login;
